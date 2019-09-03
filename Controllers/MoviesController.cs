64e1a49fa43dc8f2927c340cafe8797e342e9f0b
Richard O'Callaghan <rickojn@hotmail.com>
2019-09-03 22:30:21 +0100

    cccccccccccc

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -46,7 +46,7 @@ namespace MVCMovie.Controllers
         public ActionResult Details(int? id)
         {
             var headers = Request.Headers;
-            System.Threading.Thread.Sleep(40001);
+            System.Threading.Thread.Sleep(40002);
             var cookies = Request.Cookies;
             string correlationID = Request.Cookies["X-Correlation-ID"].Value;
             if (id == null)
